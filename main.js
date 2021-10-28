e197ecce771cf34dcd0ae829abbffb24b98f4a98
shivendra2008 <73811400+shivendra2008@users.noreply.github.com>
2021-10-28 11:34:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,53 @@
-function setup() {
+status="";
+objects=[];
+function preload(){
+}
+function setup(){
     canvas = createCanvas(480, 380);
     canvas.center();
+    
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+    input = document.getElementById("input").value;
+    console.log(input);
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    status=true;
+}
+function draw(){
+    image(video, 0, 0, 480, 380);
+    if(status != ""){
+        objectDetector.detect(video, gotResult);
+        for(i=0; i<objects.length; i++){
+            document.getElementById("status").innerHTML="Status : Objects Detected";
+            fill('#FF0000');
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y +15);
+            noFill();
+            stroke('#FF0000');
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            if (objects[i].label==input){
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("foundornot").innerHTML= input + " is found";
+               
+            }
+            else{
+                document.getElementById("foundornot").innerHTML= input + " is not found";
+                
+            }
+        }
+    }
+}
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
